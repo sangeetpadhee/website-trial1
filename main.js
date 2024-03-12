b72105d2d4a8fefb30022c3c7b529c266e2c9920
Sangeet Kumar Padhee <146935002+sangeetpadhee@users.noreply.github.com>
2024-03-12 08:06:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
+const pol = document.querySelector(".menubar");
 
 function menu(){
-    const hideMe = document.querySelector(".menu");
-    hideMe.style.display = 'block';
+  pol.style.display="block";
 }
 
-window.onload = function(){
-    
-    document.onclick = function(e){
-       var hideMe = document.querySelector(".menu");
-       if(e.target.id !== 'hideMe'){
-          hideMe.style.display = 'none';
-       }
-    };
- };
-
- var docWidth = document.documentElement.offsetWidth;
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
+window.addEventListener('mouseup',function(event){
+  if(event.target != pol && event.target.parentNode != pol){
+      pol.style.display = 'none';
   }
-);
+});  
